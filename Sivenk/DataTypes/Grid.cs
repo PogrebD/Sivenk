b044fa4c4fec4a26c07438fa569bb56dec9ac5c7
DiMs8P <dota.dimabog@mail.ru>
2024-10-23 13:30:27 +0700

    task 2

--- Sivenk/DataTypes/Grid.cs
+++ Sivenk/DataTypes/Grid.cs
@@ -14,4 +14,34 @@ public class Grid
         Elements = elements;
         Points = points;
     }
+
+    public int[] GetEdgeIds(int elementId)
+    {
+        return Elements[elementId].Edges;
+    }
+
+    public int GetEdgeId(int firstPointId, int secondPointId)
+    {
+        return 1;
+    }
+    
+    public int[] GetPointsId(int edgeId)
+    {
+        int sum = Bounds.ElementsNumX + Bounds.PointsNumX;
+
+        int[] points = new int[2];
+        if (edgeId % sum < Bounds.ElementsNumX)
+        {
+            points[0] = edgeId - (edgeId / sum * Bounds.ElementsNumX);
+            points[1] = points[0] + 1;
+        }
+        else
+        {
+            points[0] = edgeId - (edgeId / sum + 1) * Bounds.ElementsNumX;
+            points[1] = points[0] + Bounds.PointsNumX;
+        }
+        
+        Console.WriteLine($"Points: {points[0]}, {points[1]}");
+        return points;
+    }
 }
--- Sivenk/Program.cs
+++ Sivenk/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System.Globalization;
 using Sivenk;
+using Sivenk.DataTypes;
 using Sivenk.Paths;
 
 Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
 Configuration configuration = new Configuration()
 {
-    shouldSplitGrid = false,
+    shouldSplitGrid = true,
     inputPaths = [PathsProvider.InputGigaPath, PathsProvider.InputMaterialPath],
     outputPaths = [PathsProvider.OutputPointsPath, PathsProvider.OutputElementsPath],
 };
 
 Application app = new Application();
-app.Run(configuration);
+Grid grid = app.Run(configuration);
+/*grid.GetPointsId(0);
+grid.GetPointsId(25);
+grid.GetPointsId(18);*/
