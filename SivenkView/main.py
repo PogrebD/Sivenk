4f8815f0a204cb5bdf4a3a5acbb5e454a535e174
DiMs8P <dota.dimabog@mail.ru>
2024-12-19 00:47:41 +0700

    fixed figure

--- SivenkView/main.py
+++ SivenkView/main.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy.interpolate import griddata
 from matplotlib.patches import Polygon
+from matplotlib.collections import LineCollection
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
@@ -50,30 +51,20 @@ def plot_points_and_connections(points, connections):
     y_coords = [y for x, y in points.values()]
     plt.scatter(x_coords, y_coords, color='black')
 
+    lines = []
     for materialId, point1, point2, point3, point4 in connections:
-        x_values = [points[point1][0], points[point2][0]]
-        y_values = [points[point1][1], points[point2][1]]
-        plt.plot(x_values, y_values, 'black')
+        lines.extend([
+            (points[point1], points[point2]),
+            (points[point1], points[point3]),
+            (points[point3], points[point4]),
+            (points[point2], points[point4])
+        ])
 
-        x_values = [points[point1][0], points[point3][0]]
-        y_values = [points[point1][1], points[point3][1]]
-        plt.plot(x_values, y_values, 'black')
-
-        x_values = [points[point3][0], points[point4][0]]
-        y_values = [points[point3][1], points[point4][1]]
-        plt.plot(x_values, y_values, 'black')
-
-        x_values = [points[point2][0], points[point4][0]]
-        y_values = [points[point2][1], points[point4][1]]
-        plt.plot(x_values, y_values, 'black')
-
-        coords = [(points[point1][0], points[point1][1]),
-                  (points[point3][0], points[point3][1]),
-                  (points[point4][0], points[point4][1]),
-                  (points[point2][0], points[point2][1])
-                  ]
-
+        coords = [points[point1], points[point3], points[point4], points[point2]]
         draw_polygon(ax, coords, (1.0, 0.5, 0.0, 0.0))
+
+    line_collection = LineCollection(lines, colors='black', linewidths=0.8)
+    ax.add_collection(line_collection)
 
 file_path = R'C:\Users\dotad\RiderProjects\Sivenk\Sivenk\output\Result.txt'
 points_path = R'C:\Users\dotad\RiderProjects\Sivenk\Sivenk\output\points.txt'
@@ -85,15 +76,8 @@ first_value, connection_data = read_file(elements_path)
 points = read_points(point_data)
 connections = read_connections(connection_data)
 
-data = []
-with open(file_path, "r") as f:
-    for line in f:
-        x, y, value = map(float, line.split())
-        data.append((x, y, value))
-
-x_coords = [x for x, y, value in data]
-y_coords = [y for x, y, value in data]
-values = [value for x, y, value in data]
+data = np.loadtxt(file_path, usecols=(0, 1, 2))
+x_coords, y_coords, values = data[:, 0], data[:, 1], data[:, 2]
 
 grid_x, grid_y = np.mgrid[
     min(x_coords):max(x_coords):200j,
@@ -106,9 +90,6 @@ fig = plt.figure(figsize=(8, 6))
 
 plot_points_and_connections(points, connections)
 
-min_value = min(values)
-max_value = max(values)
-
 plt.imshow(
     grid_z.T,
     extent=(
@@ -118,13 +99,14 @@ plt.imshow(
         max(y_coords)
     ),
     origin="lower",
-    cmap="viridis",
-    vmin=min_value,
-    vmax=max_value
+    cmap="plasma",
+    vmin=min(values),
+    vmax=max(values)
 )
 
 plt.colorbar(label="Function value")
 plt.title("Field")
 plt.xlabel("X")
 plt.ylabel("Y")
+plt.tight_layout()
 plt.show()
